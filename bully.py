98112ded13868bc2e4201ed5d635a276361def9d
Alper ARI <alper.hamza.ari@gmail.com>
2022-12-29 13:19:45 +0300

    Implement LEADER & TERMINATE broadcast logic
    Simulation works well with provided test cases.
    Make sleeps sleep less.

--- bully.py
+++ bully.py
@@ -16,7 +16,7 @@ TIMEOUT = 5000
 
 
 # "responder" method is assigned to each process' listener_thread
-def responder(nodeId, ids_alive, pubsocket):
+def responder(nodeId, ids_alive, pubsocket, responder_return):
     print("RESPONDER STARTS", nodeId)
     
     time.sleep(1)
@@ -54,32 +54,47 @@ def responder(nodeId, ids_alive, pubsocket):
             received_body = message_parsed[0]
             received_port = int(message_parsed[1])
             sender_id = int(message_parsed[2])
-
-            print("nodeId:", nodeId, "received:", message)
+            to_id = int(message_parsed[3])
             
+            if received_body == "RESP":
+                # There is another node with higher,
+                # Become passive listener
+                if to_id == nodeId:
+                    print("nodeId:", nodeId, "received:", message)
+                    responder_return["RECEIVED_RESP"] = 1
+
+                
+                if to_id > nodeId:
+                    # Eliminate smaller passive nodes as well
+                    responder_return["RECEIVED_RESP"] = 1
+
+
             if received_body == "TERMINATE":
                 # Leader is already selected
                 # Notify main and finish myself
-                return "END"
-                break
+                print("nodeId:", nodeId, "received:", message)
+                return
             
+
             elif received_body == "LEADER":
                 # If sender_id < myid, then send "RESP" to sender
                 if sender_id < nodeId:                    
                     # TODO: send "RESP" to sender
                     
-                    # pubsocket.bind(f"tcp://127.0.0.1:{5550+nodeId}")
-                    time.sleep(2)        
+                    print("nodeId:", nodeId, "received:", message)
 
-                    resp_message = f"RESP:{5550+nodeId}:{nodeId}"
+                    # pubsocket.bind(f"tcp://127.0.0.1:{5550+nodeId}")
+                    time.sleep(1)        
+
+                    resp_message = f"RESP:{5550+nodeId}:{nodeId}:{sender_id}"
                     
                     print("nodeId:", nodeId, "sending RESP to the sender nodeId:", sender_id, "on its own port:", 5550+nodeId)
                     pubsocket.send_string(resp_message)
                     
-                    # pubsocket.disconnect(f"tcp://127.0.0.1:{received_port}")
-
-
+                    
                     # after that, notify main to broadcast "LEADER"
+                    responder_return["BROADCAST_LEADER"] = 1
+
                     # return "BROADCAST_LEADER"
                     pass
 
@@ -88,7 +103,8 @@ def responder(nodeId, ids_alive, pubsocket):
             # Then this means i am leader
             # Notify main
             print("Timeout!", nodeId)
-            return "BROADCAST_TERMINATE"
+            if not responder_return["RECEIVED_RESP"]:
+                responder_return["BROADCAST_TERMINATE"] = 1
 
 
     time.sleep(2)
@@ -105,7 +121,6 @@ def leader(nodeId, isStarter, ids_alive):
     pid = os.getpid()
     print("PROCESS STARTS ", pid, nodeId, isStarter, ids_alive)
 
-
     # Open my publisher socket 
     # (also share this pub socket with listener thread)
     port = 5550 + nodeId
@@ -114,52 +129,52 @@ def leader(nodeId, isStarter, ids_alive):
     socket = context.socket(zmq.PUB)
     socket.bind(f"tcp://127.0.0.1:{port}")
 
+    # Shared variable among 2 threads (main & listener)
+    responder_return = {"BROADCAST_LEADER": 0, "RECEIVED_RESP": 0, "BROADCAST_TERMINATE": 0}
+
     # Start listener thread listening on other ports (nodes)
-    listener_thread = threading.Thread(target=responder, args=(nodeId, ids_alive, socket,))
+    listener_thread = threading.Thread(target=responder, args=(nodeId, ids_alive, socket, responder_return,))
     listener_thread.start()
 
-    
+    # Make sure others started listening before i send LEADER
+    time.sleep(2) 
+
+
     if isStarter:
         # Broadcast 'LEADER'
+        responder_return["BROADCAST_LEADER"] = 1
+        pass
 
-        message = f"LEADER:{port}:{nodeId}"
 
-        # Make sure others started listening before i send LEADER
-        time.sleep(2) 
+    while not responder_return["BROADCAST_LEADER"]:
+        # Wait until i need to broadcast "LEADER"
+        if responder_return["RECEIVED_RESP"]:
+            break
+        pass
+    
+
+    if not responder_return["RECEIVED_RESP"] and responder_return["BROADCAST_LEADER"]:
+        time.sleep(2)
+
+        message = f"LEADER:{port}:{nodeId}:-1"
 
         print("NodeId:", nodeId, "sending message:", message)
         socket.send_string(message)
 
+        time.sleep(3)
 
 
-    responder_message = listener_thread.join()
+    if not responder_return["RECEIVED_RESP"]:
+        while not responder_return["BROADCAST_TERMINATE"]:
+            pass
+        
+        message = f"TERMINATE:{port}:{nodeId}:-1"
 
-    if responder_message == "END":
-        # Game is over
-        time.sleep(1)
+        print("NodeId:", nodeId, "sending message:", message)
 
-        return
+        socket.send_string(message)
 
-    elif responder_message == "BROADCAST_LEADER":
-        time.sleep(1)
-
-        # TODO: broadcast "LEADER"
-        return
-
-    elif responder_message == "BROADCAST_TERMINATE":
-        # Game is over, i am the leader
-        time.sleep(1)
-
-        # TODO: broadcast "TERMINATE"
-        return 
-
-
-    if isStarter:
-        pass
-    else:
-        pass
-    
-    time.sleep(1)
+    listener_thread.join()
     pass
 
 
@@ -180,9 +195,9 @@ def main(args):
     # ids_alive = random.sample(ids, numAlive)
     # ids_starter = random.sample(ids_alive, numStarter)
 
-    ids = [0,1,2,3,4,5]
-    ids_alive = [1,2,3,4]
-    ids_starter = [2]
+    ids = [0,1,2,3,4,5,6,7,8,9]
+    ids_alive = [2,4,1,9,0]
+    ids_starter = [9,4,2]
 
     print("Alives:", ids_alive, sep="\n")
     print("Starters:", ids_starter, sep="\n")
